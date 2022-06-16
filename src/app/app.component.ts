4b7ec11816b6e9afc3384175ce141b27b0af5343
Adnan Erdursun <erdursun@gmail.com>
2022-06-16 20:38:12 +0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'whats-next';
+  _title = 'whats-next';
+  _advices:string[] = [];
+
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['tr','en']);
+    translate.setDefaultLang('tr');
+  }
+
+  getAdvice() {
+    if(this._advices.length==0 || this.translate.currentLang === 'tr') {
+      this._advices = ['Piyano Çal', 'Kitap Oku', 'Futbol Oyna', 'Satranç Oyna', 'Basketbol Oyna', 'Oyuncaklarınla Oyna'];
+    }
+    if(this.translate.currentLang === 'en') {
+      this._advices = ['Play Piano', 'Read Books', 'Play football', 'Play Chess', 'Play Basketball', 'Play with Toys'];
+    }
+    return this._advices[Math.floor(Math.random() * this._advices.length)];
+  }
+
+  switchLang(lang: string) {
+    this.translate.use(lang);
+  }
 }
